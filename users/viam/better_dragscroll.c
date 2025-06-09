b72e0f30cd23f2faaa7baea27ad9cd876b67481c
plodah <mleiper89@gmail.com>
2025-06-09 03:02:57 +0100

    Option to turn off dragscroll axes from divisor menu

--- users/viam/better_dragscroll.c
+++ users/viam/better_dragscroll.c
@@ -180,9 +180,13 @@
         if (better_dragscroll_enabled_bylock || better_dragscroll_enabled_bypress) {
 
             #if defined(VIA_ENABLE) && defined(PLOOPY_VIAMENUS)
-                dragscroll_acc_h += (float)mouse_report.x / ((float)ploopyvia_config.dragscroll_divisor_h / 4);
-                dragscroll_acc_v += (float)mouse_report.y / ((float)ploopyvia_config.dragscroll_divisor_v / 4);
-            #else // defined(VIA_ENABLE) && defined(PLOOPY_VIAMENUS)
+                if(dragscroll_divisor_h > 0){
+                    dragscroll_acc_h += (float)mouse_report.x / ((float)ploopyvia_config.dragscroll_divisor_h / 4);
+                }
+                if(dragscroll_divisor_v > 0){
+                    dragscroll_acc_v += (float)mouse_report.y / ((float)ploopyvia_config.dragscroll_divisor_v / 4);
+                }
+                #else // defined(VIA_ENABLE) && defined(PLOOPY_VIAMENUS)
                 dragscroll_acc_h += (float)mouse_report.x / BETTER_DRAGSCROLL_DIVISOR_H;
                 dragscroll_acc_v += (float)mouse_report.y / BETTER_DRAGSCROLL_DIVISOR_V;
             #endif // defined(VIA_ENABLE) && defined(PLOOPY_VIAMENUS)
