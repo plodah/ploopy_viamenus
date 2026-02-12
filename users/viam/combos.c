befc69a4920f66071d100bde78ad33e64fde8c5d
plodah <plodah@pm.me>
2026-02-12 01:55:41 +0000

    reduce combo count if not rp2040

--- users/viam/combos.c
+++ users/viam/combos.c
@@ -2,59 +2,71 @@
   #pragma once
 
     const uint16_t PROGMEM pl_combo_ms12[] = {MS_BTN1, MS_BTN2, COMBO_END};
-    const uint16_t PROGMEM pl_combo_ms15[] = {MS_BTN1, MS_BTN5, COMBO_END};
     const uint16_t PROGMEM pl_combo_ms24[] = {MS_BTN2, MS_BTN4, COMBO_END};
-    const uint16_t PROGMEM pl_combo_ms25[] = {MS_BTN2, MS_BTN5, COMBO_END};
-
-    const uint16_t PROGMEM pl_combo_ms15_lt1[] = {MS_BTN1 , LT(1, MS_BTN5), COMBO_END};
-    const uint16_t PROGMEM pl_combo_ms15_lt2[] = {MS_BTN1 , LT(2, MS_BTN5), COMBO_END};
     const uint16_t PROGMEM pl_combo_ms24_lt1[] = {MS_BTN2 , LT(1, MS_BTN4), COMBO_END};
-    const uint16_t PROGMEM pl_combo_ms25_lt1[] = {MS_BTN2 , LT(1, MS_BTN5), COMBO_END};
-    const uint16_t PROGMEM pl_combo_ms25_lt2[] = {MS_BTN2 , LT(2, MS_BTN5), COMBO_END};
 
-    const uint16_t PROGMEM pl_combo_ms1d[] = {MS_BTN1, BETTER_DRAG_SCROLL_MOMENTARY, COMBO_END};
-    const uint16_t PROGMEM pl_combo_ms2d[] = {MS_BTN2, BETTER_DRAG_SCROLL_MOMENTARY, COMBO_END};
+    #if defined(PLOOPY_COBMO_EXCESSIVE)
+        const uint16_t PROGMEM pl_combo_ms15[] = {MS_BTN1, MS_BTN5, COMBO_END};
+        const uint16_t PROGMEM pl_combo_ms25[] = {MS_BTN2, MS_BTN5, COMBO_END};
+
+        const uint16_t PROGMEM pl_combo_ms15_lt1[] = {MS_BTN1 , LT(1, MS_BTN5), COMBO_END};
+        const uint16_t PROGMEM pl_combo_ms15_lt2[] = {MS_BTN1 , LT(2, MS_BTN5), COMBO_END};
+        const uint16_t PROGMEM pl_combo_ms25_lt1[] = {MS_BTN2 , LT(1, MS_BTN5), COMBO_END};
+        const uint16_t PROGMEM pl_combo_ms25_lt2[] = {MS_BTN2 , LT(2, MS_BTN5), COMBO_END};
+
+        const uint16_t PROGMEM pl_combo_ms1d[] = {MS_BTN1, BETTER_DRAG_SCROLL_MOMENTARY, COMBO_END};
+        const uint16_t PROGMEM pl_combo_ms2d[] = {MS_BTN2, BETTER_DRAG_SCROLL_MOMENTARY, COMBO_END};
+    #endif // defined(PLOOPY_COBMO_EXCESSIVE)
 
     #if defined(COMMUNITY_MODULE_MOUSE_JIGGLER_ENABLE)
-      const uint16_t PROGMEM pl_combo_ms14[] = {MS_BTN1, MS_BTN4, COMBO_END};
-      const uint16_t PROGMEM pl_combo_ms14_lt1[] = {MS_BTN1 , LT(1, MS_BTN4), COMBO_END};
-      const uint16_t PROGMEM pl_combo_ms45[] = {MS_BTN4, MS_BTN5, COMBO_END};
-      const uint16_t PROGMEM pl_combo_ms45_lt01[] = {MS_BTN4 , LT(1, MS_BTN5), COMBO_END};
-      const uint16_t PROGMEM pl_combo_ms45_lt02[] = {MS_BTN4 , LT(2, MS_BTN5), COMBO_END};
-      const uint16_t PROGMEM pl_combo_ms45_lt10[] = {LT(1, MS_BTN4) , MS_BTN5, COMBO_END};
-      const uint16_t PROGMEM pl_combo_ms45_lt11[] = {LT(1, MS_BTN4) , LT(1, MS_BTN5), COMBO_END};
-      const uint16_t PROGMEM pl_combo_ms45_lt12[] = {LT(1, MS_BTN4) , LT(2, MS_BTN5), COMBO_END};
+        const uint16_t PROGMEM pl_combo_ms14[] = {MS_BTN1, MS_BTN4, COMBO_END};
+        const uint16_t PROGMEM pl_combo_ms14_lt1[] = {MS_BTN1 , LT(1, MS_BTN4), COMBO_END};
+        #if defined(PLOOPY_COBMO_EXCESSIVE)
+            const uint16_t PROGMEM pl_combo_ms45[] = {MS_BTN4, MS_BTN5, COMBO_END};
+            const uint16_t PROGMEM pl_combo_ms45_lt01[] = {MS_BTN4 , LT(1, MS_BTN5), COMBO_END};
+            const uint16_t PROGMEM pl_combo_ms45_lt02[] = {MS_BTN4 , LT(2, MS_BTN5), COMBO_END};
+            const uint16_t PROGMEM pl_combo_ms45_lt10[] = {LT(1, MS_BTN4) , MS_BTN5, COMBO_END};
+            const uint16_t PROGMEM pl_combo_ms45_lt11[] = {LT(1, MS_BTN4) , LT(1, MS_BTN5), COMBO_END};
+            const uint16_t PROGMEM pl_combo_ms45_lt12[] = {LT(1, MS_BTN4) , LT(2, MS_BTN5), COMBO_END};
+        #endif // defined(PLOOPY_COBMO_EXCESSIVE)
     #endif // defined(COMMUNITY_MODULE_MOUSE_JIGGLER_ENABLE)
 
     combo_t key_combos[] = {
-      #if defined(TAP_DANCE_ENABLE) && defined(BETTER_DRAGSCROLL_TAPDANCE)
-        COMBO(pl_combo_ms12,  TD(TD_DRAGSCROLL)),
-      #else // BETTER_DRAGSCROLL_TAPDANCE
-        COMBO(pl_combo_ms12,  BETTER_DRAG_SCROLL_MOMENTARY),
-      #endif // BETTER_DRAGSCROLL_TAPDANCE
-      COMBO(pl_combo_ms15,  C(KC_C)),
-      COMBO(pl_combo_ms24,  BETTER_DRAG_SCROLL_MOMENTARY),
-      COMBO(pl_combo_ms25,  C(KC_V)),
+        #if defined(TAP_DANCE_ENABLE) && defined(BETTER_DRAGSCROLL_TAPDANCE)
+            COMBO(pl_combo_ms12,  TD(TD_DRAGSCROLL)),
+        #else // BETTER_DRAGSCROLL_TAPDANCE
+            COMBO(pl_combo_ms12,  BETTER_DRAG_SCROLL_MOMENTARY),
+        #endif // BETTER_DRAGSCROLL_TAPDANCE
+        COMBO(pl_combo_ms24,  BETTER_DRAG_SCROLL_MOMENTARY),
+        COMBO(pl_combo_ms24_lt1,  BETTER_DRAG_SCROLL_MOMENTARY),
 
-      COMBO(pl_combo_ms15_lt1,  C(KC_C)),
-      COMBO(pl_combo_ms15_lt2,  C(KC_C)),
-      COMBO(pl_combo_ms24_lt1,  BETTER_DRAG_SCROLL_MOMENTARY),
-      COMBO(pl_combo_ms25_lt1,  C(KC_V)),
-      COMBO(pl_combo_ms25_lt2,  C(KC_V)),
+        #if defined(PLOOPY_COBMO_EXCESSIVE)
+            COMBO(pl_combo_ms15,  C(KC_C)),
+            COMBO(pl_combo_ms25,  C(KC_V)),
+            COMBO(pl_combo_ms15_lt1,  C(KC_C)),
+            COMBO(pl_combo_ms15_lt2,  C(KC_C)),
+            COMBO(pl_combo_ms25_lt1,  C(KC_V)),
+            COMBO(pl_combo_ms25_lt2,  C(KC_V)),
 
-      COMBO(pl_combo_ms1d,  BETTER_DRAG_SCROLL_TOGGLE),
-      COMBO(pl_combo_ms2d,  BETTER_DRAG_SCROLL_TOGGLE),
+            COMBO(pl_combo_ms1d,  BETTER_DRAG_SCROLL_TOGGLE),
+            COMBO(pl_combo_ms2d,  BETTER_DRAG_SCROLL_TOGGLE),
+        #endif // defined(PLOOPY_COBMO_EXCESSIVE)
 
-      #if defined(COMMUNITY_MODULE_MOUSE_JIGGLER_ENABLE)
-        COMBO(pl_combo_ms14,  CM_MSJG),
-        COMBO(pl_combo_ms14_lt1,  CM_MSJG),
-        COMBO(pl_combo_ms45,  CM_MSJG),
-        COMBO(pl_combo_ms45_lt01,  CM_MSJG),
-        COMBO(pl_combo_ms45_lt02,  CM_MSJG),
-        COMBO(pl_combo_ms45_lt10,  CM_MSJG),
-        COMBO(pl_combo_ms45_lt11,  CM_MSJG),
-        COMBO(pl_combo_ms45_lt12,  CM_MSJG),
-      #endif // COMMUNITY_MODULE_MOUSE_JIGGLER_ENABLE
+
+        #if defined(COMMUNITY_MODULE_MOUSE_JIGGLER_ENABLE)
+
+            COMBO(pl_combo_ms14,  CM_MSJG),
+            COMBO(pl_combo_ms14_lt1,  CM_MSJG),
+            #if defined(PLOOPY_COBMO_EXCESSIVE)
+
+                COMBO(pl_combo_ms45,  CM_MSJG),
+                COMBO(pl_combo_ms45_lt01,  CM_MSJG),
+                COMBO(pl_combo_ms45_lt02,  CM_MSJG),
+                COMBO(pl_combo_ms45_lt10,  CM_MSJG),
+                COMBO(pl_combo_ms45_lt11,  CM_MSJG),
+                COMBO(pl_combo_ms45_lt12,  CM_MSJG),
+            #endif // defined(PLOOPY_COBMO_EXCESSIVE)
+        #endif // COMMUNITY_MODULE_MOUSE_JIGGLER_ENABLE
     };
 
     #if defined(VIA_ENABLE) && defined(PLOOPY_VIAMENUS) && defined(COMBO_SHOULD_TRIGGER)
--- users/viam/common.h
+++ users/viam/common.h
@@ -4,6 +4,7 @@
 #define PLOOPY_VIAMENUS
 #define BETTER_DRAGSCROLL
 
+// set number of layers- 8 if rp2040, else 4
 #if ! defined(DYNAMIC_KEYMAP_LAYER_COUNT)
     #if defined(QMK_MCU_RP2040)
         #define DYNAMIC_KEYMAP_LAYER_COUNT 8
@@ -11,6 +12,13 @@
         #define DYNAMIC_KEYMAP_LAYER_COUNT 4
     #endif
 #endif // DYNAMIC_KEYMAP_LAYER_COUNT
+
+// Some combos only on rp2040
+#if defined(QMK_MCU_RP2040)
+    #if (!defined(PLOOPY_COBMO_EXCESSIVE))
+        #define PLOOPY_COBMO_EXCESSIVE
+    #endif // PLOOPY_COBMO_EXCESSIVE
+#endif // QMK_MCU_RP2040
 
 // Some keycode aliases to make the declarations look prettier.
 // Kind of pointless since the point of this repo is to define in Via. :shrug:
