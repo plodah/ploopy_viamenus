928356ebb0ac6347f247f1a436e13ca1d59dd71d
plodah <github@plodah.uk>
2026-07-18 03:23:40 +0100

    more rgb effects on rp2040-based devices

---
+++ keyboards/ploopyco/trackball_thumb/rev1_002/keymaps/viam_rgb/keymap.c
@@ -0,0 +1,82 @@
+/* Copyright 2025 Plodah
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+
+enum layers {
+    _BASE,
+    _FNA,
+    _FNB,
+};
+
+enum keymap_keycodes {
+    KB_DPI_CONFIG       = QK_KB_0,
+    KB_DRAG_SCROLL,
+    BETTER_DRAG_SCROLL_MOMENTARY,
+    BETTER_DRAG_SCROLL_TOGGLE,
+    PL_MSJG,
+    PL_TSKN,
+    PL_TSKP,
+    DPI_SNIPER_A_MOMENTARY,
+    DPI_SNIPER_A_TOGGLE,
+    DPI_SNIPER_B_MOMENTARY,
+    DPI_SNIPER_B_TOGGLE,
+    BETTER_DRAG_ACTION_A_MOMENTARY,
+    BETTER_DRAG_ACTION_B_MOMENTARY,
+    PMW_CCW,
+    PMW_CW,
+    PMW_RST,
+    TBO_TG1,
+    TBO_MO1,
+    TBO_TG2,
+    TBO_MO2,
+    TBO_TG3,
+    TBO_MO3,
+    TBO_TG4,
+    TBO_MO4,
+    MOR_MAN,
+    MOR_DIT,
+    MOR_DSH,
+};
+
+enum {
+    TD_DRAGSCROLL,
+};
+
+#include "common.c"
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BASE] = LAYOUT(
+        P_MS4FA, MS_BTN1, MS_BTN3, MS_BTN2, P_MS5FB,
+        DPI_CONFIG
+    ),
+    [_FNA] = LAYOUT(
+        _______, PL_TSKP, _______, PL_TSKN, _______,
+        PL_MSJG
+    ),
+    [_FNB] = LAYOUT(
+        _______, _______, _______, _______, _______,
+        _______
+    )
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [_BASE] = {ENCODER_CCW_CW(MS_WHLD, MS_WHLU)},
+    [_FNA] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [_FNB] = {ENCODER_CCW_CW(_______, _______)}
+};
+#endif // ENCODER_MAP_ENABLE
